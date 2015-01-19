41a2a94f19da99ba84b4b01ea7712bd5cbaa5330
lpgn <git@xinfab.com>
2015-01-19 08:20:14 +0800

    pin correction

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -1794,8 +1794,8 @@
 #ifndef SDSUPPORT
 // these pins are defined in the SD library if building with SD support
   #define SCK_PIN          9
-  #define MISO_PIN         10
-  #define MOSI_PIN         11
+  #define MISO_PIN         11
+  #define MOSI_PIN         10
 #endif
 
 #ifdef ULTRA_LCD
